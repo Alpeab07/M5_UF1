9b21bbe38da250a4937b2037095374586d8e92e3
Alex <alexpeab@gmail.com>
2022-06-07 17:19:38 +0200

    prova

---
+++ M5UF1AlejandroPerezAbeleira/src/main/java/dam2/aperez/M5UF1AlejandroPerezAbeleira/HolaMon.java
@@ -0,0 +1,13 @@
+package dam2.aperez.M5UF1AlejandroPerezAbeleira;
+
+/**
+ * Hello world!
+ *
+ */
+public class HolaMon 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "hola món" );
+    }
+}
